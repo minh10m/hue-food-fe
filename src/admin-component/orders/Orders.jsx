1b6cc52be752946a8333ef1b38bc2febbaca177b
minh10m <minhhh1010@gmail.com>
2025-11-10 22:54:10 +0700

    feat(order): add page

--- src/admin-component/orders/Orders.jsx
+++ src/admin-component/orders/Orders.jsx
@@ -17,33 +17,9 @@ export const Orders = () => {
   }
 
   return (
-    <div className='px-2'>
-  <Card className='p-5'>
-    <Typography sx={{ paddingBottom: "1rem" }} variant="h5">
-      Order Status
-    </Typography>
-
-    <FormControl>
-      <RadioGroup
-        onChange={handleFilter}
-        row
-        name="category"
-        value={filterValue || ""}
-      >
-        {orderStatus.map((item) => <FormControlLabel
-          key={item.label}
-          value={item.value}
-          control={<Radio/>}
-          label={item.label}
-          sx={colors.grey}
-          
-        />)}
-      </RadioGroup>
-    </FormControl>
-  </Card>
 
   <OrderTable/>
-</div>
+
 
   )
 }
